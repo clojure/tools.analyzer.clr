60f8d7fe49194337eaaf8e7c5638e173bcb4e835
David Miller <dmiller2718@gmail.com>
2022-11-15 08:27:38 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# tools.analyzer.clr
+
+A port of [clojure/tools.analyzer.jvm](https://github.com/clojure/tools.analyzer.jvm] to ClojureCLR.
+
+THIS IS A WORK-IN-PROGRESS.  DO NOT USE. (YET.)
+
+From the parent's README:
+
+> An analyzer for Clojure code, written on top of [tools.analyzer](https://github.com/clojure/tools.analyzer), providing additional jvm-specific passes.
+
+Please see the JVM version's [README](https://github.com/clojure/tools.analyzer.jvm/blob/master/README.md) for more details.
+
+# Releases
+
+Nuget reference:
+
+    PM> Install-Package tools.analyzer.clr -Version 1.2.2 
+	
+Leiningen/Clojars reference:
+
+   [org.clojure.clr/tools.analyzer.clr "1.1.1"]
+   
+## Notes on the ported code ##
+
+Work in progress.
+
+## License
+
+Copyright (C) 2022 Rich Hickey, David Miller & ClojureCLR contributors
+Distributed under the Eclipse Public License, the same as Clojure.
+
+
+The parent project has this:
+
+>Copyright © 2013-2021 Nicola Mometto, Rich Hickey & contributors.
+>Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ project.clj
@@ -0,0 +1,32 @@
+(defproject org.clojure.clr/tools.analyzer.clr "1.2.2"
+  :description "Port of clojure.org/tools.analyzer.clr to ClojureCLR"
+  :url "https://github.com/clojure/tools.analyzer.clr"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/main/clojure" "src/main/lib"]
+  :test-paths ["src/test/clojure"]			
+  :dependencies [[org.clojure/tools.analyzer "1.1.0"]
+                 [org.clojure.clr/core.memoize "1.0.257"]
+				 [org.clojure.clr/tools.reader "1.3.6"]]
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo/"
+                                    :sign-releases  false}]]  
+  :min-lein-version "2.0.0"
+  :plugins [[lein-clr "0.2.0"]]
+  :clr {:cmd-templates  {:clj-exe   [#_"mono" [CLJCLR15_40 %1]]
+                         :clj-dep   [#_"mono" ["target/clr/clj/Debug 4.0" %1]]
+                         :clj-url   "https://github.com/downloads/clojure/clojure-clr/clojure-clr-1.4.1-Debug-4.0.zip"
+                         :clj-zip   "clojure-clr-1.4.1-Debug-4.0.zip"
+                         :curl      ["curl" "--insecure" "-f" "-L" "-o" %1 %2]
+                         :nuget-ver [#_"mono" [*PATH "nuget.exe"] "install" %1 "-Version" %2]
+                         :nuget-any [#_"mono" [*PATH "nuget.exe"] "install" %1]
+                         :unzip     ["unzip" "-d" %1 %2]
+                         :wget      ["wget" "--no-check-certificate" "--no-clobber" "-O" %1 %2]}
+        ;; for automatic download/unzip of ClojureCLR,
+        ;; 1. make sure you have curl or wget installed and on PATH,
+        ;; 2. uncomment deps in :deps-cmds, and
+        ;; 3. use :clj-dep instead of :clj-exe in :main-cmd and :compile-cmd
+        :deps-cmds      [ ;[:wget  :clj-zip :clj-url] ; edit to use :curl instead of :wget
+                          ;[:unzip "../clj" :clj-zip]
+                         ]
+        :main-cmd      [:clj-exe "Clojure.Main.exe"]
+        :compile-cmd   [:clj-exe "Clojure.Compile.exe"]})
