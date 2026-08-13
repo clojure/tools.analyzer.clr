2e79c13d619d0ce4eb173d6bebf85de8d823d120
David Miller <dmiller2718@gmail.com>
2026-08-13 09:32:13 -0400

    Update to Java commit d61a85c (2026.02.13): don't try to fix it with ., just keep as invoke of method-values for now.  NOTE: this broke 29 tests. I may have to back this change out later.  I'm going to see if the next few commits fixes the problems.

--- src/main/clojure/clojure/tools/analyzer/clr.clj
+++ src/main/clojure/clojure/tools/analyzer/clr.clj
@@ -129,11 +129,10 @@
                          (maybe-class-literal sym-ns))]
       (let [opname (name form)
             opsym (symbol opname)]
-
         (cond
           ;; Array/<n>, leave as is
           (and (= (count opname) 1)
-               (Char/IsDigit (char (first opname))))                                                              ;;; Character/isDigit
+               (Char/IsDigit (char (first opname))))                                                                   ;;; Character/isDigit
           form
 
           ;; Class/.method or Class/new, leave as is to be parsed as :maybe-host-form -> :method-value
@@ -163,47 +162,33 @@
                              (not (resolve-ns (symbol opns) env))
                              (maybe-class-literal opns))]
 
-          (let [param-tags (param-tags-of op)
-                form-meta (cond-> (meta form)
-                            param-tags (assoc :param-tags param-tags))]
-            (cond
-              ;; (Class/new args) -> (new Class args)
-              (= "new" opname)
-              (with-meta (list* 'new target expr)
-                form-meta)
+          (cond
+           ;; (Class/new args), (Class/.method target args), (^[pt] Class/method args)
+           ;; -> leave as-is, will be analyzed as invoke of method-value
+           (or (= "new" opname)
+               (.StartsWith ^String opname ".")                                                       ;;; .startsWith
+               (param-tags-of op))
+           form
 
-              ;; (Class/.method target args) -> (. ^Class (do target) (method rest-args))
-              (.StartsWith ^String opname ".")                                                     ;;; .startsWith
-              (if (seq expr)
-                (let [method-sym (symbol (subs opname 1))
-                      [target-arg & args] expr]
-                  (with-meta (list '. (with-meta (list 'do target-arg)
-                                        {:tag target})
-                                   (if (seq args)
-                                     (list* method-sym args)
-                                     method-sym))
-                    form-meta))
-                form)
-
-              ;; (Class/method args) -> (. Class (method args))
-              :else
-              (let [op-sym (symbol opname)]
-                (with-meta (list '. target (if (seq expr)
-                                             (list* op-sym expr)
-                                             op-sym))
-                  form-meta))))
+           ;; (Class/method args) -> (. Class (method args))
+           :else
+           (let [op-sym (symbol opname)]
+             (with-meta (list '. target (if (seq expr)
+                                          (list* op-sym expr)
+                                          op-sym))
+               (meta form))))
 
           (cond
            (.StartsWith opname ".")     ; (.foo bar ..)                                                  ;;; .startsWith
            (let [[target & args] expr
                  target (if-let [target (maybe-class-literal target)]
                           (with-meta (list 'do target)
-                                     {:tag 'System.Type})                                                ;;; java.lang.Class
+                            {:tag 'System.Type})                                                ;;; java.lang.Class
                           target)
                  args (list* (symbol (subs opname 1)) args)]
              (with-meta (list '. target (if (= 1 (count args)) ;; we don't know if (.foo bar) is
                                           (first args) args))  ;; a method call or a field access
-                        (meta form)))
+               (meta form)))
 
            (.EndsWith opname ".") ;; (class. ..)                                                          ;;; .endsWith
            (with-meta (list* 'new (symbol (subs opname 0 (dec (count opname)))) expr)
